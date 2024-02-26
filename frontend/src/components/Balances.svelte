e639e752142fcd7e4235b0638bd235e4cf5faeb2
firebat75 <jacklowry1337@gmail.com>
2024-02-26 18:53:20 -0500

    added balances component

---
+++ frontend/src/components/Balances.svelte
@@ -0,0 +1,15 @@
+<script>
+    let wallets = {};
+    fetch("http://localhost:3042/balances")
+        .then((response) => response.json())
+        .then((data) => {
+            wallets = data;
+            for (const [key, value] of Object.entries(wallets)) {
+                console.log(`${key}: ${value}`);
+            }
+        });
+</script>
+
+{#each Object.entries(wallets) as [address, amount]}
+    <p>{address}: {amount}</p>
+{/each}
---
+++ frontend/src/lib/stores.js
@@ -0,0 +1,3 @@
+import { writable } from 'svelte/store';
+
+export const address = writable({ "0x1"});
