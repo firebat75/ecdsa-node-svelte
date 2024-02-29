97dbc8da45f089363242aa53eef656d942aec996
firebat75 <jacklowry1337@gmail.com>
2024-02-29 12:28:46 -0500

    removed dependancies for removed components

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script>
     import AddressBook from "../components/AddressBook.svelte";
-    import BitsTest from "../components/BitsTest.svelte";
     import CreateWallet from "../components/CreateWallet.svelte";
     import Transfer from "../components/Transfer.svelte";
-    import { Button } from "bits-ui";
 </script>
 
 <div class="p-4">
