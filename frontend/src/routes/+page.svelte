0ee608ebd2c5888fb44794982c8d5a8b8ebcf7d1
firebat75 <jacklowry1337@gmail.com>
2024-03-06 17:33:17 -0500

    changed title style

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,8 +4,10 @@
     import Transfer from "../components/Transfer.svelte";
 </script>
 
-<div class="p-4">
-    <h1 class="m-2">ECDSA Node</h1>
+<div class="grid justify-center p-4">
+    <div class="m-2 h-min w-min bg-slate-700 rounded justify-self-center">
+        <h1 class="m-2 text-center">ECDSA Node</h1>
+    </div>
 
     <div class="flex justify-center w-full">
         <div class="w-min">
